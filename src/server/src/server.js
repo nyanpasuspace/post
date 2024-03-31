7faa3c69457ce8f11a677648096d10a9d9923012
maodaisuki <nyanyanyapu@gmail.com>
2024-03-31 21:28:02 +0800

    feat: 接口与分层

--- src/server/src/server.js
+++ src/server/src/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const { resolve } = require('path');
 const { promisify } = require('util');
+const initControllers = require('./controllers');
 
 const server = express();
 const port = parseInt(process.env.PORT || '9000');
@@ -9,6 +10,7 @@ const publicDir = resolve('public');
 
 async function bootstrap() {
   server.use(express.static(publicDir));
+  server.use(await initControllers());
   await promisify(server.listen.bind(server, port))();
   console.log(`> Started on port ${port}`);
 }
